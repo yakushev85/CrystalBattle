2c12540c05be8776f4e9ee7206b5205840d63599
yakushev85 <hrip85@gmail.com>
2023-02-23 11:47:33 +0200

    update controls for player

--- src/objects/Player.gd
+++ src/objects/Player.gd
@@ -1,29 +1,38 @@
 extends KinematicBody2D
 
 export var speed = 100
-var screen_size
+var generated_velocity = Vector2.ZERO
+var event_position = Vector2.ZERO
 
+func _ready():
+	$AnimatedSprite.animation = "idle"
+	$AnimatedSprite.playing = true
 
 func _process(delta):
-	var velocity = Vector2.ZERO # The player's movement vector.
-	if Input.is_action_pressed("ui_right"):
-		velocity.x += 1
-	if Input.is_action_pressed("ui_left"):
-		velocity.x -= 1
-	if Input.is_action_pressed("ui_down"):
-		velocity.y += 1
-	if Input.is_action_pressed("ui_up"):
-		velocity.y -= 1
-	
-	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed
+	if generated_velocity.length() > 0:
+		generated_velocity = generated_velocity.normalized() * speed
 		
-		$AnimatedSprite.flip_h = velocity.x < 0
+		$AnimatedSprite.flip_h = generated_velocity.x < 0
 		
 		$AnimatedSprite.animation = "run"
+		
+		generated_velocity = generated_velocity * delta
+	
+		var collision_obj = move_and_collide(generated_velocity)
+		
+		var dest_vector = event_position - position
+		var dest_distance = sqrt(dest_vector.x*dest_vector.x + dest_vector.y*dest_vector.y)
+		
+		if collision_obj != null or dest_distance < 1:
+			generated_velocity = Vector2.ZERO
 	else:
 		$AnimatedSprite.animation = "idle"
 	
-	velocity = velocity * delta
-	
-	move_and_collide(velocity)
+
+func _input(event):
+	if event is InputEventMouseButton and not (event as InputEventMouseButton).is_pressed():
+		event_position = event.position
+		generated_velocity = event_position - position
+		var norm_size = max(abs(generated_velocity.x), abs(generated_velocity.y))
+		
+		generated_velocity = generated_velocity / norm_size
